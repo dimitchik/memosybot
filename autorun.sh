a96d4a288f800a747962daeb383097b8b0ab0133
dimitchik <dimitchik@gmail.com>
2024-01-17 12:50:53 +0200

    fixes to scripts

--- autorun.sh
+++ autorun.sh
@@ -1,2 +1,6 @@
-screen -X -S botupdater quit
+# Check if the screen session exists
+if screen -list | grep -q "botupdater"; then
+    # Kill the screen session
+    screen -X -S botupdater quit
+fi
 screen -dmS botupdater ./autoupdate.sh
--- restart.sh
+++ restart.sh
@@ -1,2 +1,6 @@
-screen -X -S bot quit
-screen -dmS bot python memosybot.py
+# Check if the screen session exists
+if screen -list | grep -q "botrunner"; then
+    # Kill the screen session
+    screen -X -S botrunner quit
+fi
+screen -dmS botrunner ./autoupdate.sh
