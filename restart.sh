97f45e14ebe8f6487c5733a5bfabae7e5c10290b
Dmytro Burkanov <dimitchik@gmail.com>
2024-01-17 14:18:03 +0200

    fix for restart script

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ if screen -list | grep -q "botrunner"; then
     # Kill the screen session
     screen -X -S botrunner quit
 fi
-screen -dmS botrunner ./autoupdate.sh
+screen -dmS botrunner python3 memosybot.py
